97ac090d4915a2c54e222226eb129a3ceaa73a80
fidanbb <fidanbb@code.edu.az>
2024-08-12 22:56:26 +0400

    Add project files.

---
+++ Services/Catalog/MultiShopWithMicroservice.Catalog/Dtos/ProductDetailDtos/CreateProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShopWithMicroservice.Catalog.Dtos.ProductDetailDtos
+{
+    public class CreateProductDetailDto
+    {
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        public string ProductId { get; set; }
+    }
+}
