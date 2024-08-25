5dcbae7baa9f04ca920c43353a22da45f07aaa46
fidanbb <fidanbb@code.edu.az>
2024-08-25 19:14:48 +0400

    added home page design

---
+++ Frontends/MultiShopWithMicroservice.WebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShopWithMicroservice.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
